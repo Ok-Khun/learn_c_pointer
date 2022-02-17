8824aa8a3eced3e44aaff012ccc2ff2a58314d0e
khun okkar <okkar.khuno08@gmail.com>
2022-02-17 23:36:27 +0630

    pointers and 2d arrays

--- 7_pointers_and_2d_arr.c
+++ 7_pointers_and_2d_arr.c
@@ -1,15 +1,33 @@
 #include<stdio.h>
 
 int main(){
-    int arr[5] = {1,2,3,4,5};
-    int *p;
-    p = arr;
-    // address of array
-    printf("%p\n", arr);
-    printf("%p\n", p);
-    // value of array
-    printf("%i\n", arr[0]);
-    printf("%i\n", arr[2]);
-    printf("%i\n", *p);
-    printf("%i\n", *(p+1));
+    int arr[2][3] = {
+        {1,2,3},
+        {4,5,6}
+    };
+
+    // int (*p)[3] = arr; 
+
+    // address of arr[0][0]
+    printf("%p \n", arr);
+    printf("%p \n", &arr[0]);
+    // printf("%p \n", p);
+    printf("%p \n", *arr);
+    printf("\n");
+
+    // address of arr[1][0]
+    printf("%p \n", arr[1]);
+    printf("%p \n", &arr[1][0]);
+    printf("%p \n", arr + 1);
+    printf("%p \n", *(arr+1));
+
+    // value arr[0][1]
+    printf("%i \n", *(*arr + 1));
+
+    // value arr[0][1]
+    printf("%i \n", *(*(arr + 1) + 1));
+
+    /*
+    arr[i][j] = *(arr[i] + j) <or> *(*(B+i) + j)
+    */
 }

7c8fe8f1edda1eb82e085912b3f2a5c1e947d9d8
khun okkar <okkar.khuno08@gmail.com>
2022-02-20 20:17:51 +0630

    update

--- 4_pointers_as_func_args.c
+++ 4_pointers_as_func_args.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 
-void increment(int* a){
-    *a = *a + 1;
+void increment(int *p)
+{
+    *p = *p + 1;
 }
 
-int main(){
+int main()
+{
+    // Pointer as function argument
     int a = 10;
+    // int* p = &a;
+    // increment(p);
+    // or
     increment(&a);
-    printf("a = %i \n", a);
+    
+    printf("%i \n", a);
 }
